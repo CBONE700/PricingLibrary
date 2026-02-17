96b9af22ddd6503e8ba0611f90cec3a57d2523de
Callum Preston <CallumCPreston@outlook.com>
2026-02-17 21:56:37 +0000

    Create simple main function and gitignore file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include "derivatives_pricing/options_pricing/black_scholes_model/black_scholes_model.h"
 using namespace std;
 
 int main()
 {
   cout << "Welcome to the derivatives pricing engine\n";
-
+  Option::option_type type = Option::option_type::call;
+  BlackScholesModel bsm = BlackScholesModel(100, 1, 100, 0.2, 0.05, type);
+  cout << "Option price: " << bsm.get_price() << endl;
   
-
   return 0;
 }
