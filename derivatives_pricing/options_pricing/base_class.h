d1af5bf9391d11811c0f94e0c0ccadb4c9673325
Callum Preston <CallumCPreston@outlook.com>
2026-02-17 21:48:31 +0000

    Implement Black Scholes Model Functions

--- derivatives_pricing/options_pricing/base_class.h
+++ derivatives_pricing/options_pricing/base_class.h
@@ -7,12 +7,12 @@ class Option : public Derivative {
 public:
   enum class option_type { call, put };
   // Default option pricing constructor
-  explicit Option(double strike, double maturity, double underlying_price, double volatility, double risk_free_rate, option_type type)
-  : _strike(strike), _maturity(maturity), _underlying_price(underlying_price), _volatility(volatility), _risk_free_rate(risk_free_rate), _option_type(type) {}
+  explicit Option(double strike, double time_to_expiration, double underlying_price, double volatility, double risk_free_rate, option_type type)
+  : _strike(strike), _time_to_expiration(time_to_expiration), _underlying_price(underlying_price), _volatility(volatility), _risk_free_rate(risk_free_rate), _option_type(type) {}
 
   //Setter methods for all input variables
   void set_strike(double value) {_strike = value;}
-  void set_maturity(double value) {_maturity = value;}
+  void set_time_to_expiration(double value) {_time_to_expiration = value;}
   void set_underlying_price(double value) {_underlying_price = value;}
   void set_volatility(double value) {_volatility = value;}
   void set_risk_free_rate(double value) {_risk_free_rate = value;}
@@ -21,15 +21,15 @@ public:
 
   //Getter methods for all input variables
   double get_strike() const { return _strike; }
-  double get_maturity() const { return _maturity; }
+  double get_time_to_expiration() const { return _time_to_expiration; }
   double get_underlying_price() const { return _underlying_price; }
   double get_volatility() const { return _volatility; }
   double get_risk_free_rate() const { return _risk_free_rate; }
   option_type get_option_type() const {return _option_type; }
-private:
+protected:
   // Inputs into all option pricing models
   double _strike;
-  double _maturity;
+  double _time_to_expiration;
   double _underlying_price;
   double _volatility;
   double _risk_free_rate;
--- derivatives_pricing/options_pricing/black_scholes_model/black_scholes_model.h
+++ derivatives_pricing/options_pricing/black_scholes_model/black_scholes_model.h
@@ -7,13 +7,14 @@ class BlackScholesModel : public Option {
   double d1;
   double d2;
 
+  double _normal_cdf(double d) const;
+  double _calculate_d1(double underlying_price, double strike, double time_to_expiration, double volatility, double risk_free_rate);
+  double _calculate_d2(double d1, double volatility, double time_to_expiration);
+
 public:
-  BlackScholesModel(double strike, double maturity, double underlying_price, double volatility, double risk_free_rate, option_type type);
+  explicit BlackScholesModel(double strike, double time_to_expiration, double underlying_price, double volatility, double risk_free_rate, option_type type);
+  double get_d1() const { return d1; }
+  double get_d2() const { return d2; }
+  double get_price() const override;
 
-  double normal_cdf(double d) const;
-  double calculate_d1();
-  double calculate_d2();
-
-  const double& get_d1() const;
-  const double& get_d2() const;
 };
