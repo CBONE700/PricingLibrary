c7920ebf6a8fcbf2f29c719e547bedd92cfd4e7b
Callum Preston <CallumCPreston@outlook.com>
2026-02-21 13:38:06 +0000

    change test value back to regular

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
   cout << "Welcome to the derivatives pricing engine\n";
   pricing_engine::instruments::option_type type = pricing_engine::instruments::option_type::call;
   pricing_engine::instruments::Option option = pricing_engine::instruments::Option(100, 1, type);
-  pricing_engine::market_data::MarketData market_data = pricing_engine::market_data::MarketData(-100, 0.05, 0.2);
+  pricing_engine::market_data::MarketData market_data = pricing_engine::market_data::MarketData(100, 0.05, 0.2);
   pricing_engine::models::BlackScholesModel bsm_engine;
   double bsm_price = bsm_engine.price(option, market_data);
   cout << "Option price: " << bsm_price << endl;
