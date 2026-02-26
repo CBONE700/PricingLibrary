0a7a56b13d6617e6810bd2f4c6636921a8b7b4dd
Callum Preston <CallumCPreston@outlook.com>
2026-02-26 21:20:48 +0000

    add Antithetic variates

--- src/monte_carlo_model.cpp
+++ src/monte_carlo_model.cpp
@@ -24,25 +24,39 @@ double pricing_engine::models::MonteCarloModel::compute_price(
     double dt = option.get_time_to_expiration() / steps;
     double nudt = (market_data.get_interest_rate() - 0.5 * market_data.get_volatility() * market_data.get_volatility()) * dt;
     double volsdt = market_data.get_volatility() * std::sqrt(dt);
+    double lnS = std::log(market_data.get_spot_price());
 
     std::mt19937 local_rng = rng;
     std::normal_distribution<double> distribution(0.0, 1.0);
 
     double sum_Ct = 0.0;
+    double sum_Ct2 = 0.0;
 
     for (int j = 0; j < paths; ++j) {
-      double lnSt = std::log(market_data.get_spot_price());
+      double lnSt1 = lnS;
+      double lnSt2 = lnS;
         for (int i = 0; i < steps; ++i) {
-            lnSt += nudt + volsdt * distribution(local_rng);
+          lnSt1 += nudt + volsdt * distribution(local_rng);
+          lnSt2 += nudt + volsdt * distribution(local_rng);
         }
-        double St = std::exp(lnSt);
-        double Ct = std::max(
+        double St1 = std::exp(lnSt1);
+        double St2 = std::exp(lnSt2);
+        double Ct = 0.5 * (
+          std::max(
             0.0,
             (option.get_type() == pricing_engine::instruments::option_type::call)
-                ? (St - option.get_strike())
-                : (option.get_strike() - St)
+                ? (St1 - option.get_strike())
+                : (option.get_strike() - St1)
+          ) +
+          std::max(
+            0.0,
+            (option.get_type() == pricing_engine::instruments::option_type::call)
+                ? (St2 - option.get_strike())
+                : (option.get_strike() - St2)
+          )
         );
         sum_Ct += Ct;
+        sum_Ct2 += Ct * Ct;
     }
 
     return std::exp(-market_data.get_interest_rate() * option.get_time_to_expiration()) * (sum_Ct / paths);
